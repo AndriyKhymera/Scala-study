4a634e787f92f29b594f88b4522d0e71f7921078
Khymera, Andrii <ext-andrii.khymera@here.com>
2020-07-16 09:17:42 +0300

    Trying some definitions and structure

---
+++ src/main/scala/semantic/Hello2.scala
@@ -0,0 +1,16 @@
+package semantic
+
+//App is trait that wraps everything in main method
+object Hello2 extends App {
+  print("Hello Scala")
+//  define variable
+  val var1 = "String1"
+  var var2 = "String2"
+//  define variable with explicitly specifying the type
+  val var3: String = "String3"
+  var var4: String = "String4"
+//  defaults to Int type
+  val number1 = 1
+//  defaults to Double type
+  val number2 = 2.0
+}
